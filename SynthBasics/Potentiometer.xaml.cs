8503945b2a14912b8e091e6198fd141cebd2636b
Tom Nantais <tomnantais@gmail.com>
2016-11-15 08:51:07 -0500

    convert a click on the black ring to a pot value

--- SynthBasics/Potentiometer.xaml.cs
+++ SynthBasics/Potentiometer.xaml.cs
@@ -19,6 +19,8 @@ namespace SynthBasics
     /// </summary>
     public partial class Potentiometer : UserControl
     {
+        static private readonly Point _center = new Point { X = 200, Y = 200 };
+
         public Potentiometer()
         {
             InitializeComponent();
@@ -43,41 +45,51 @@ namespace SynthBasics
             return false;
         }
 
-        /*
         private static void CurrentSettingChanged(DependencyObject depObj, DependencyPropertyChangedEventArgs args)
         {
             Potentiometer c = (Potentiometer)depObj;
-
-            Label theLabel = c.numberDisplay;
-
-            theLabel.Content = args.NewValue.ToString();
-
-            double proportion = (double)(args.NewValue) / 100.0;
-
-            c.thumb.SetValue(Canvas.LeftProperty, proportion * c.sliderbkgnd.ActualWidth);
-        }
-         */ 
-
-        private static void CurrentSettingChanged(DependencyObject depObj, DependencyPropertyChangedEventArgs args)
-        {
-            Potentiometer c = (Potentiometer)depObj;
-            Point center = new Point { X = 200, Y = 200 };
             double radius = 175; //circular arc has overall diameter of 370, but that includes a stroke width of 20 pixels
             double angleinradians;
             Point newpos;
 
-            //convert a value from 0 to 100 to an angle (remembering that +ve X and Y are down and to the right)
+            //convert a value from 0 to 100 to an angle from -225 to +45 (remembering that +ve X and Y are down and to the right)
             angleinradians = (2.65 * ((double) args.NewValue) - 225) * Math.PI / 180.0;
 
-            newpos = new Point(center.X + radius * Math.Cos(angleinradians), center.Y + radius * Math.Sin(angleinradians));
+            newpos = new Point(_center.X + radius * Math.Cos(angleinradians), _center.Y + radius * Math.Sin(angleinradians));
 
             //subtract half the width and height of the dot to place the top-left corner
-            newpos.X -= c.thumb.ActualWidth / 2;
-            newpos.Y -= c.thumb.ActualHeight / 2;
+            newpos.X -= c.thumb.Width / 2;
+            newpos.Y -= c.thumb.Height / 2;
 
             c.thumb.SetValue(Canvas.LeftProperty, newpos.X);
             c.thumb.SetValue(Canvas.TopProperty, newpos.Y);
         }
+
+        private void Path_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Point pos = e.GetPosition((IInputElement) this); //get position relative to pot
+            double newsetting = ConvertRingPositionToPotSetting(pos);
+            MessageBox.Show("new value: " + newsetting.ToString());
+        }
+
+        private double ConvertRingPositionToPotSetting(Point posInRing)
+        {
+            double potvalue = 0;
+
+            //The angle range runs from 45 (fully CW) to -225 (fully CCW) because Y increases downward
+            double angleinradians = Math.Atan2(posInRing.Y - _center.Y, posInRing.X - _center.X);
+            double angleindegrees = angleinradians * 180 / Math.PI;
+            if (angleindegrees > 45)
+            {
+                angleindegrees -= 360;
+            }
+
+            potvalue = ((angleindegrees + 225) / 270) * 100; 
+
+            return potvalue;
+        }
+
+
         
     }
 }
